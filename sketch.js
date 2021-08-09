fefd98da92d08f3f345daeb37b0c611f7c33eba2
VikilLegend <88488370+VikilLegend@users.noreply.github.com>
2021-08-09 08:21:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+function setup() {
+  createCanvas(400,400);
+background(30);
+}
+function draw() 
+{
+ 
+if(keyIsDown(DOWN_ARROW))  {
+
+background('red');
+
+}
+
+if(keyIsDown(TOP_ARROW))  {
+
+background('green');
+
+}
+
+if(keyIsDown(LEFT_ARROW))   {
+
+background('orange');
+
+}
+
+if(keyIsDown(RIGHT_ARROW))  {
+
+background('blue');
+
+}
+
+
+}
+
+
+
+
